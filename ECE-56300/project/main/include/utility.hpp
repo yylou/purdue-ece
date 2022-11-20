7c6eece419f2cbf97e49571bcf05ed0e4ff52c9d
Arno3165229 <48749946+Arno3165229@users.noreply.github.com>
2022-11-19 20:56:49 -0800

    Revert "Adding Lock"

---
+++ ECE-56300/project/main/include/utility.hpp
@@ -0,0 +1,17 @@
+/*
+Author  : Yuan-Yao Lou (Mike)
+Title   : PhD student in ECE at Purdue University
+Email   : yylou@purdue.edu
+Website : https://yylou.github.io/
+Date    : 2022/11/11
+
+Project : Purdue ECE56300 - Project
+Version : v1.0 (utility header file)
+*/
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int compareString(char* src, const char* target);
+void log(std::string message, std::string argu, int skip);
---
+++ ECE-56300/project/user-Arnold/README.md
@@ -0,0 +1,20 @@
+## Directory Structure
+``` python
+user-Arnold
+├── 
+├── 
+├── 
+├── 
+├──
+├──
+└── README.md
+```
+
+## Setup
+``` shell
+1. 
+2. 
+3. 
+4. 
+5. 
+```
